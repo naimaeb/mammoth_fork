ca877f740479639c705528fefd378e13caf56182
naimaeb <naima.elosegui@gmail.com>
2024-12-20 16:59:49 +0100

    models for gvcl

---
+++ models/gvcl_curved.py
@@ -0,0 +1,20 @@
+class CurvedGVCL(ContinualModel):
+    def __init__(self):
+        super().__init__()
+        # Add any additional initialization code here
+
+    def forward(self, x):
+        # Add your forward pass implementation here
+        pass
+
+    def backward(self, loss):
+        # Add your backward pass implementation here
+        pass
+
+    def update(self):
+        # Add your update implementation here
+        pass
+
+    def evaluate(self, x, y):
+        # Add your evaluation code here
+        pass
